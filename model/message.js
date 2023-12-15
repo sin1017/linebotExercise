88b059f75e8de179b077425e41aa54ec23eef473
sin1017 <catmoon1017@gmail.com>
2023-12-15 14:25:49 +0800

    feat: 🎸 修改testMessage改成oder並測試html tag 寫入訊息排版

--- model/message.js
+++ model/message.js
@@ -1,6 +1,11 @@
-const testMessage = {
+const oder = {
 	type: 'text',
-	text: '已收到訊息，回傳測試訊息',
+	text: `<p>|查詢|</p> 
+       <p>查詢+月份:查詢該月份休假名單及天數</p>
+       <p>查詢名單:查詢現職員工名單</p>
+    <p>|新增|</p>
+       <p>註冊會員:註冊發出訊息的員工資料</p>
+       <p>休假2023/12/15:新增休假</p>`,
 };
 
-module.exports = testMessage;
+module.exports = [oder];
