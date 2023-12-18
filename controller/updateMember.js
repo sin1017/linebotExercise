f06b0276f77871dba13af3ad269067a4a421a044
sin1017 <catmoon1017@gmail.com>
2023-12-18 15:29:28 +0800

    fix: 🐛 再次修改 select db response log content

--- controller/updateMember.js
+++ controller/updateMember.js
@@ -35,7 +35,8 @@ async function deleteMember(userinfo) {
 
 		// const deleteOrder = `UPDATE zeabur.user SET status = '1' WHERE (id = ${dbId})`;
 		// "DELETE FROM `zeabur`.`user` WHERE (`id` = '1') and (`uid` = '123');";
-		const dbId = await db.query(selectDbId)[0][0].id;
+		const [result, filed] = await db.query(selectDbId);
+		const dbId = result[0].id;
 		// dbId = result.id
 		console.log('result ----', dbId);
 		return true;
