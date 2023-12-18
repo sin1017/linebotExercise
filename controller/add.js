bd4c6a09a2ccdb5f4ea209e9fe7bce9ecd810e2b
sin1017 <catmoon1017@gmail.com>
2023-12-18 10:12:58 +0800

    fix: 🐛 修改寫入 uid 參數

--- controller/add.js
+++ controller/add.js
@@ -3,14 +3,13 @@ const db = require('../database/database');
 async function addMember(userinfo) {
 	// console.log('userinfo:::::', userinfo);
 	try {
-		const dbOrder = `INSERT INTO zeabur.user (uid, name) VALUES (${userinfo.id}, '歆偊')`;
+		const dbOrder = `INSERT INTO zeabur.user (uid, name) VALUES (${userinfo.source.userId}, '歆偊')`;
 		const deleteOrder =
 			"DELETE FROM `zeabur`.`user` WHERE (`id` = '1') and (`uid` = '123');";
 		const [rows, fields] = await db.execute(dbOrder);
 		return rows;
 	} catch (err) {
 		return false;
-		console.log('失敗了', err);
 	}
 }
 
