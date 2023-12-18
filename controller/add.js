33736f44e690b2b21acf82438af9b0712edaa924
sin1017 <catmoon1017@gmail.com>
2023-12-18 09:59:17 +0800

    fix: 🐛 修改log 嘗試取得line 回傳 user id

--- controller/add.js
+++ controller/add.js
@@ -3,11 +3,11 @@ const db = require('../database/database');
 async function addMember(userinfo) {
 	console.log('userinfo:::::', userinfo);
 	try {
-		// const dbOrder = `INSERT INTO 'zeabur'.'user' ('id', 'uid', 'name') VALUES ('1', '123', '歆偊');`;
-		const dbOrder = `INSERT INTO zeabur.user (id, uid, name) VALUES ('1', '123', '歆偊')`;
+		const dbOrder = `INSERT INTO zeabur.user (uid, name) VALUES (${userinfo.id}, '歆偊')`;
 		const deleteOrder =
 			"DELETE FROM `zeabur`.`user` WHERE (`id` = '1') and (`uid` = '123');";
 		const [rows, fields] = await db.execute(dbOrder);
+		// console.log('rows :::: db request', rows);
 	} catch (err) {
 		console.log('失敗了', err);
 	}
