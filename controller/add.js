2d51fdf58bbfe62bed2dd7e3d5487c3b469ddce5
sin1017 <catmoon1017@gmail.com>
2023-12-18 09:36:35 +0800

    fix: 🐛 修改寫入sql 語法

--- controller/add.js
+++ controller/add.js
@@ -3,7 +3,8 @@ const db = require('../database/database');
 async function addMember(userinfo) {
 	console.log('userinfo:::::', userinfo);
 	try {
-		const dbOrder = `INSERT INTO 'zeabur'.'user' ('id', 'uid', 'name') VALUES ('1', '123', '歆偊');`;
+		// const dbOrder = `INSERT INTO 'zeabur'.'user' ('id', 'uid', 'name') VALUES ('1', '123', '歆偊');`;
+		const dbOrder = `INSERT INTO zeabur.user (id, uid, name) VALUES ('1', '123', '歆偊')`;
 		const deleteOrder =
 			"DELETE FROM `zeabur`.`user` WHERE (`id` = '1') and (`uid` = '123');";
 		const [rows, fields] = await db.execute(dbOrder);
